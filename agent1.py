a342ea4c779c6076950627b9557102812b108919
yp2024az-jpg <yp2024az@gmail.com>
2025-09-09 17:56:22 +0530

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -7,8 +7,23 @@ from dotenv import load_dotenv
 import faiss
 from langchain_core.runnables import RunnableParallel, RunnableLambda , RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+import os
+from langchain_core.tools import tool
+import requests
+from urllib.parse import urlparse, parse_qs  
+
 load_dotenv()
-video_id = "hmtuvNfytjM" 
+
+def get_video_id(youtube_url):
+    parsed_url = urlparse(youtube_url)
+    query_params = parse_qs(parsed_url.query)
+    return query_params.get("v", [None])[0]
+youtube_url= input("Enter youtube video url:")
+video_id = get_video_id(youtube_url)
+if not video_id:
+    print("Invalid YouTube URL. Please try again.")
+    exit()
+
 try:
     transcript_list1 = YouTubeTranscriptApi()
     transcript_list = transcript_list1.fetch(video_id)
@@ -17,13 +32,14 @@ except TranscriptsDisabled:
     print("No captions available for this video.")
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 text=text_splitter.create_documents([transcript])
-emmbedings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+emmbedings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001",google_api_key=os.getenv("GOOGLE_API_KEY"))
 vector_store = FAISS.from_documents(text, emmbedings)
 reteiver=vector_store.as_retriever(search_type="similarity", search_kwargs={"k":4})
-llm= ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.2)
+llm= ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.2,google_api_key=os.getenv("GOOGLE_API_KEY"))
 prompt=PromptTemplate(
-    template="""you are a helpful assistant. Use the following context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Context: {context} 
-    Question: {question}""",
+    template="""You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. keep the answer concise.
+    Question: {question} 
+    Context: {context}""",
     input_variables=["context", "question"]
 )
 def format_docs(retieved_docs):
@@ -34,5 +50,6 @@ parallel_chain=RunnableParallel({
     "question" : RunnablePassthrough()
 })
 parser=StrOutputParser()
+st=input("")
 chain1=parallel_chain|prompt|llm|parser
-print(chain1.invoke("who is speaking in the video?"))
+print(chain1.invoke(st))
